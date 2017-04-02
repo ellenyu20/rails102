012ffc98d35da1b7e159705e46cc7df6e88fa649
ellenyu20 <evetteyu20@gmail.com>
2017-04-02 21:56:42 +0800

    enable post editing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,12 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!, :only => [:new, :create]
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    # 生成编辑文章的动态url路径
+  end
+
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -20,10 +26,23 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    #以上为路径
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+      #指向我发布的文章路径
+    else
+      render :edit
+      #校验不为空
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
   end
-
 end
